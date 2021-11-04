a6a917d90cbe56d95e54767893177d205cc83260
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-11-04 22:17:45 +0700

    install packages, init styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Row>
+        <header class="pb-3 mb-4 mt-3 border-bottom">
+          <a
+            href="/"
+            class="d-flex align-items-center text-dark text-decoration-none"
+          >
+            <span class="fs-4">The Movie Search</span>
+          </a>
+        </header>
+      </Row>
+      <Row>
+        <div class="p-5 mb-4 bg-light rounded-3">
+          <Container fluid className="py-5">
+            <h1 class="display-5 fw-bold">Welcome to The Movie Search</h1>
+            <Col md="8" className="fs-4">
+              Find your favorite Movies or Series here.
+            </Col>
+          </Container>
+        </div>
+      </Row>
+    </Container>
+  )
 }
 
-export default App;
+export default App
